25d8288794fa62053ac1a98bbdfcb6ca4e6500ab
Bhargav S M <smbhargav2004@gmail.com>
2025-06-21 00:18:40 +0530

    Update token.did.d.ts

--- src/declarations/token/token.did.d.ts
+++ src/declarations/token/token.did.d.ts
@@ -4,4 +4,5 @@ export interface _SERVICE {
   'getSymbol' : () => Promise<string>,
   'payOut' : () => Promise<string>,
   'transfer' : (arg_0: Principal, arg_1: bigint) => Promise<string>,
+  'getListedNFTPrice': (arg_0: Principal) => Promise<bigint>,
 }
