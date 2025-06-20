5f994efe7826c4000ece4f1f87face8b6d79eed9
Bhargav S M <smbhargav2004@gmail.com>
2025-06-21 00:19:08 +0530

    Update token.did.js

--- src/declarations/token/token.did.js
+++ src/declarations/token/token.did.js
@@ -4,6 +4,7 @@ export const idlFactory = ({ IDL }) => {
     'getSymbol' : IDL.Func([], [IDL.Text], ['query']),
     'payOut' : IDL.Func([], [IDL.Text], []),
     'transfer' : IDL.Func([IDL.Principal, IDL.Nat], [IDL.Text], []),
+    'getListedNFTPrice': IDL.Func([IDL.Principal], [IDL.Nat], ['query']),
   });
 };
 export const init = ({ IDL }) => { return []; };
